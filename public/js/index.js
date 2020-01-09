d9235214b7b46b40a3e22bf71f3376d493a2e0e8
booldook <booldook@gmail.com>
2020-01-09 09:38:10 +0900

    fadeInOut완성

--- public/js/index.js
+++ public/js/index.js
@@ -13,8 +13,17 @@ console.log(		$(".banner").eq(2)		);
 var now = 2;
 setInterval(bannerToggle, 3000);
 function bannerToggle() {
+	//now = (now == 2) ? 0 : now + 1;
 	if(now == 2) now = 0;
 	else now++;
 	$(".banner").hide();
 	$(".banner").eq(now).show();
+}
+
+var now2 = 2;
+setInterval(bannerFade, 3000);
+function bannerFade() {
+	now2 = (now2 == 2) ? 0 : now2 + 1;
+	$(".banner2").stop().fadeOut(1000);
+	$(".banner2").eq(now2).stop().fadeIn(1000);
 }
